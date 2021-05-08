1d73daddf6313f7abbb97c6133667ef35b97c59d
“fanhuanxi” <fanhuanxi@meituan.com>
2021-05-08 11:39:30 +0800

    管理资源

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,26 @@ module.exports = {
   mode: 'production',
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
-    rules: [{ test: /\.txt$/, use: 'raw-loader' }]
+    rules: [
+      // 为css文件配置loader
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      // 为图像文件配置loader
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      // 为字体文件配置loader
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
   },
-  plugins: [new HtmlWebpackPlugin({template: './src/index.html'})],
 }
